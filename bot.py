d09fa896b7c5e7680b7334c7aa006b7de27a75e6
Peter <pfphilly2020@gmail.com>
2023-09-10 14:05:10 -0400

    We have our first command

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import responses
 import os
 from dotenv import load_dotenv
@@ -17,24 +18,23 @@ def run_discord_bot():
     #print(os.environ.get('BallsBotToken'))
     intents= discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
+    client = commands.Bot(description="Balls", command_prefix='/B ', intents=intents, help_command=None)
     
 
     @client.event
     async def on_ready():
         print(f"{client.user} is now running")
     
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        
-        username = str(message.author)
-        user_message = str(message.content)
+    @client.command()
+    async def about(ctx):
+        embed = discord.Embed(title="About this Lovely Bot",
+                              description="Catch countryballs and trade with others. maybe even fight your friends, for honor and for GLORY.",
+                              color=discord.Color.blurple())
+        embed.add_field(name="More Commands", value="When i get the time to add commands they will be here.")
+        embed.add_field(name="Thanks to my dads", value="Thank you to my dads Peter and Jonathan")
+        await ctx.send(embed=embed)
 
-        print(f"{username} said {user_message}")
-
-        await send_message(message, user_message)
+    
 
 
     client.run(TOKEN)
