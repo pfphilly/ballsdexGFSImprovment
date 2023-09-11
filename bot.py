32a1308747646acaf1a8c5d54de9f9d1e4368257
Peter <pfphilly2020@gmail.com>
2023-09-11 19:44:52 -0400

    it sends images

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import responses
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv('.env')
 
@@ -12,6 +13,30 @@ async def send_message(message, user_message):
         await message.channel.send(response)
     except Exception as e:
         print("excepton" + e)
+
+#class SimpleView(discord.ui.View)
+
+async def spawnBall(message):
+    
+    
+    with open('balls.json', 'r') as f:
+        embed = discord.Embed(
+        title="Quick, Somebody, Catch this wild countryball!", 
+        color=discord.Color.random()
+        )
+        data = json.load(f)
+        flagpath = data['1']["flag"]
+        embed.set_image(url=f"attachment://{flagpath}")
+        view = discord.ui.View()
+        button=discord.ui.Button(label="CATCH")
+        view.add_item(button)
+        #embed.set_image(file=discord.File(flagpath))
+        #print(flagpath)
+        if message.channel.name == 'alphatesting':
+            await message.channel.send(embed=embed, file=discord.File(flagpath), view=view)
+    #embed.set_image()
+    #await message.channel.send
+
 
 def run_discord_bot():
     TOKEN = os.getenv('TOKEN')
@@ -24,6 +49,14 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f"{client.user} is now running")
+
+    @client.event
+    async def on_message(message):
+        if message.author.display_name == client.user.display_name:
+            return
+        
+        if last_spawn == "":
+            await spawnBall(message)
     
     @client.command()
     async def about(ctx):
@@ -33,7 +66,7 @@ def run_discord_bot():
         embed.add_field(name="More Commands", value="When i get the time to add commands they will be here.")
         embed.add_field(name="Thanks to my dads", value="Thank you to my dads Peter and Jonathan")
         await ctx.send(embed=embed)
-
+    last_spawn = ""
     
 
 
